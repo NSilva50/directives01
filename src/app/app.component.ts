c27a2598d2d2c386b6c3577a8b372d503e18e242
User <nathansilva50@gmail.com>
2018-06-13 14:07:00 -0700

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  buttonText: string = 'Display Details';
+  paragraphText: string = 'Favorite Food: ';
+
+  buttonStart: string = 'Display Details';
+  paragraphStart: string = 'Favorite Food: ';
+
+  buttonAlternative: string = 'Hide Details';
+  paragraphAlternative: string = 'Pizza and Panfried Chow-mein';
+
+  start() {
+    this.buttonText = this.buttonStart;
+    this.paragraphText = this.paragraphStart;
+  }
+
+  toggle() {
+    if(this.buttonText === this.buttonStart){
+      this.buttonText = this.buttonAlternative;
+      this.paragraphText = this.paragraphAlternative;
+    } else{
+      this.buttonText = this.buttonStart;
+      this.paragraphText = this.paragraphStart;
+    }
+    console.log();
+  }
 }
